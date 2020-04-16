d528a5ea64d13644452447d33bbb871cb0c87bf9
ereninal10@gmail.com <ereninal10@gmail.com>
2020-04-16 23:02:14 +0300

    Entity Framework using database created.

---
+++ Migrations/20200416195200_InitialCreate.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace first_app_net_core.Migrations
+{
+    public partial class InitialCreate : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Categories",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    name = table.Column<string>(maxLength: 100, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Categories", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Products",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Name = table.Column<string>(maxLength: 100, nullable: false),
+                    Price = table.Column<decimal>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Products", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Categories");
+
+            migrationBuilder.DropTable(
+                name: "Products");
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,7 +1,37 @@
 ﻿using System;
+using System.ComponentModel.DataAnnotations;
+using Microsoft.EntityFrameworkCore;
 
 namespace first_app_net_core
 {
+    public class ShopContext : DbContext
+    {
+        public DbSet<Product> Products {get; set;}
+        public DbSet<Category> Categories { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlite("Data Source=Shop.db");
+        }
+    }
+    public class Product
+    {
+        public int Id { get; set; }
+        [Required]
+        [MaxLength(100)]
+        public string Name { get; set; }
+        public decimal? Price { get; set; }
+
+
+    }
+    public class Category
+    {
+        public int Id { get; set; }
+        [Required]
+        [MaxLength(100)]
+        public string name { get; set; }
+
+    }
     class Program
     {
         static void Main(string[] args)
