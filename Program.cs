54f7ac35a94131a290766155030ec91419370e16
ereninal10@gmail.com <ereninal10@gmail.com>
2020-04-16 22:25:03 +0300

    Project file created.

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace first_app_net_core
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.ReadLine();
+            
+        }
+    }
+}
