c18983164149a5e13a9dcb4e60bd269d2229b110
ereninal10@gmail.com <ereninal10@gmail.com>
2020-04-18 14:14:44 +0300

    Project done.

--- Program.cs
+++ Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
+using System.Linq;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Logging;
 
@@ -42,7 +43,11 @@ namespace first_app_net_core
     {
         static void Main(string[] args)
         {
-            AddProducts();
+            //AddProducts();
+            //GetAllProducts();
+            //GetProductById(2);
+            //UpdateProduct(1);
+            DeleteProduct(5);
             
         }
         static void AddProducts()
@@ -53,6 +58,57 @@ namespace first_app_net_core
                 db.Products.Add(products);
                 db.SaveChanges();
                 Console.WriteLine("Veriler Eklendi.");
+            }
+        }
+        static void GetAllProducts()
+        {
+            using(var db = new ShopContext())
+            {
+                var products = db.Products.Select(p => new {
+                    p.Name,
+                    p.Price
+                }).ToList();
+                foreach (var item in products)
+                {
+                    Console.WriteLine($"Name :{item.Name} Price :{item.Price}");
+
+                }
+            }
+        }
+        static void GetProductById(int id)
+        {
+            using(var db = new ShopContext())
+            {
+                string msj ="";
+                var result = db.Products.
+                Where(p => p.Id == id)
+                .FirstOrDefault();//Sorgu sonucu bir eleman döneceği için dönüşte eğer sorgu karlığı boş ise null döndürür
+                msj = result == null ? msj ="Not id values" : msj =$"Name :{result.Name} Price :{result.Price}";
+                Console.WriteLine(msj);
+
+                
+            }
+        }
+        static void UpdateProduct(int id)
+        {
+            using(var db = new ShopContext())
+            {
+                var query = new Product(){Id = id};
+                db.Products.Attach(query);
+                query.Price =4000;
+                db.SaveChanges();
+                Console.WriteLine("Entry updated");
+            }
+        }
+        static void DeleteProduct(int id)
+        {
+            using(var db = new ShopContext())
+            {
+                var qeury = new Product(){Id = id};
+                //db.Products.Remove(qeury);
+                db.Entry(qeury).State = EntityState.Deleted;
+                db.SaveChanges();
+                Console.WriteLine("Entry deleted");
             }
         }
     }
